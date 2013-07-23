85c16ca4706745b8ac37312b817ae5e09b1ddee1
Eric Allen <eric.justin.allen@gmail.com>
2013-07-23 18:27:40 -0500

    changes... WAY TOO MANY OF THEM

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,18 +3,16 @@ get '/' do
 end
 
 post '/enter_race' do
- @player1 = Player.find_or_create_by_player_name(params[:player_one])
- @player2 = Player.find_or_create_by_player_name(params[:player_two])
-  
-  if @player1.errors.any? || @player2.errors.any?
-    erb :index
-  else
-    erb :race
+	p params
+  @players = params[:player_names].map do |player|
+  	Player.find_or_create_by_player_name(player)
   end
+  erb :race
 end
 
 post '/game_over' do
   @game = Game.create( {:player_ids => params[:player_ids],
-                        :winner_id => params[:winner_id]})
+                        :winner_id => params[:winner_id],
+                        :game_duration => params[:game_duration]})
   erb :index
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,39 +1,100 @@
-function advancePlayer(player) {
-  player.removeClass("active").next().addClass("active");
+// This is the constructor function way of creating a JS Object.
+function Player(id, keycode, name){
+  this.id = id;
+  this.keycode = keycode;
+  this.name = name;
 }
-$(document).ready(function (){
-  $('.reset').on('click', function(){
+
+Player.prototype.advance = function(){
+  var playerTrack = $('#player' + this.id);
+  var playerPosition = playerTrack.find('li.active')
+  playerPosition.removeClass("active").next().addClass("active");
+}
+
+// This is the literal notation way of creating a JS Object.
+// The literal notation is preferred for namespacing.
+var Game = {
+  playerArray: [],
+  startTime: null,
+  endTime: null,
+  gameIsOver: function() {
+    return $('li:last-child').hasClass('active');
+  },
+
+  winner: function() {
+    return $('ol').find('li:last-child.active').parent().data('player-id');
+  },
+
+  persistGameResults: function(players, winner_id, elapsed_time) {
+    $.post('/game_over', {player_ids: this.playerArray.map(function(player){ return player.id; }),
+      winner_id: this.winner(),
+      game_duration: this.elapsedTime()});
+  },
+
+  findPlayerById: function(player_id){
+    return this.playerArray.filter( function(player) {
+      return (player.id == player_id);
+    })[0];
+  },
+
+  findPlayerByKeyCode: function(keyCode) {
+    return this.playerArray.filter( function(player) {
+      return (player.keycode == keyCode);
+    })[0];
+  },
+  started: function() {
+    return this.startTime !== null;
+  },
+  start: function() {
+    this.startTime = new Date();
+  },
+  finish: function() {
+    this.endTime = new Date();
+    $('#reset').removeClass('hidden');
+    $('#winner').html(this.findPlayerById(winner()).name);
+  },
+  elapsedTime: function() {
+    return (this.endTime - this.startTime) / 1000;
+  },
+  handleKeyUp: function(event){
+    var keycode = event.which;
+
+    if(!Game.started()) {
+      Game.start();
+    }
+
+    var player = Game.findPlayerByKeyCode(keycode);
+    player.advance();
+
+    if (Game.gameIsOver()) {  //Handle keyup event
+      Game.finish();
+      $(document).off('keyup');
+      Game.persistGameResults();
+    }
+  },
+
+  loadGameFromDom: function(){
+    $(document).on('keyup', this.handleKeyUp);
+
+    var eachTrack = $('ol.track');
+
+    for(var i = 0; i < eachTrack.length; i++){
+      var newPlayerId = $(eachTrack[i]).data('player-id');
+      var newPlayerKeyCode = $(eachTrack[i]).data('player-key');
+      var newPlayerName = $(eachTrack[i]).data('player-name');
+      this.playerArray.push(new Player(newPlayerId, newPlayerKeyCode, newPlayerName));
+    }
+  }
+};
+
+$(document).ready(function() {
+  Game.loadGameFromDom();
+  $('.reset').on('click', function() { //can pull this function out and name it
     location.reload();
-  });
-
-  $(document).on('keyup', function(event){
-    var keycode = (event.keyCode? event.keyCode :event.which);
-    if(keycode == '81'){
-      var player = $('#player1');
-      advancePlayer( player.find('.active'));
-
-      if( $('#player1 li:last-child').hasClass('active')) {
-        var winnerId = player.attr('data-player-id');
-        var params = { player_ids: window.playerIds, winnder_id: winnerId };
-        $.post('/game_over', params);
-        $('#notify_winner').html('Player 1 Wins!');
-        $('.reset').removeClass('hidden');
-        $(document).off('keyup');
-      }
-    }
-
-    if(keycode == '80'){
-      var player = $('#player2');
-      advancePlayer( player.find('.active'));
-
-      if( $('#player2 li:last-child').hasClass('active')) {
-        var winnerId = player.attr('data-player-id');
-        var params = { player_ids: window.playerIds, winnder_id: winnerId };
-        $.post('/game_over', params);
-        $('#notify_winner').html('Player 2 Wins!');
-        $('.reset').removeClass('hidden');
-        $(document).off('keyup');
-      }
-    }
-  });
+  })
 });
+
+
+
+
+
